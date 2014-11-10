ec983fbc27d3d935320e4d22ee4c2442c84d19f8
LukeStorry <luke@lukestorry.co.uk>
2014-11-10 15:45:49 +0000

    added first functions

--- main.c
+++ main.c
@@ -1,20 +1,40 @@
-/* a matrix calcualtor
+/* a matrix calculator
  * Written by Luke Storry
  * https://github.com/LukeStorry/Matrices/
+ *
+ *
+ * matrices stored in a linked list
  */
 
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+
+//define a new type to store the matrices in.
+typedef struct matrix{
+    char name;
+    float ** arr;
+    struct matrix *next;
+}matrix;
+
+//define function to input matrix
+matrix inputMatrix (void) {
+         
+}
 
 
-#define MAXNOOFARRAYS 26
-#define MAXSIZEOFARRAYS 50
-
-
+//function to return matrix from just the "name"
+matrix *findMatrix(char name, matrix *listStart) {
+    matrix ptr = listStart;
+    while (ptr->name != name && ptr != NULL) { //end-of-list error-check?
+	ptr = ptr->next;
+    };
+    return ptr;
+};
 
 int main (void) {
-
-
+    matrix *listStart;
+    
+    
+    return 0;
 }
 
