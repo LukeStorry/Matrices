3476e3aec201d863b183993c7a7d666c9a486ff3
LukeStorry <luke@lukestorry.co.uk>
2014-11-10 13:53:59 +0000

    basic outline

---
+++ main.c
@@ -0,0 +1,20 @@
+/* a matrix calcualtor
+ * Written by Luke Storry
+ * https://github.com/LukeStorry/Matrices/
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+#define MAXNOOFARRAYS 26
+#define MAXSIZEOFARRAYS 50
+
+
+
+int main (void) {
+
+
+}
+
